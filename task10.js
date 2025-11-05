a5f0e36a45f55d9fc0530ec36ca620120ee6d805
Denis Harrison <denharrison797@gmail.com>
2025-11-05 18:42:07 +0300

    added task 10

---
+++ task10.js
@@ -0,0 +1,16 @@
+const array = [
+  { name: "width", value: 123 },
+  { name: "height", value: 222 },
+];
+
+const obj = array.reduce((object, value ) => {
+  // object - аккумулятор (начинается с {})
+  // value - текущий объект из массива
+  // index - индекс (0, затем 1)
+
+  object[value.name] = value.value;
+  return object;
+  
+}, {});
+
+console.log(obj); // { width: 123, height: 222 }
